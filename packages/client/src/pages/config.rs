9887e54e9149c4f60c37743560b8ddc119f766e4
伊欧 <langyo.china@gmail.com>
2024-10-02 20:32:13 +0800

    ✨ Use `yuuka` to generate configure structures automatic.

--- packages/client/src/pages/config.rs
+++ packages/client/src/pages/config.rs
@@ -13,7 +13,6 @@ pub fn ConfigPage() -> HtmlResult {
         None
     })?
     .unwrap();
-    let global_config = use_state(|| (*global_config).clone().unwrap_or_default());
 
     let global_state = use_context::<GlobalStateContext>().expect("Global state not found");
 
@@ -47,182 +46,186 @@ pub fn ConfigPage() -> HtmlResult {
                 box-shadow: var(--shadow-half);
             ")}>
                 {
-                    [
-                        (
-                            t.config.portal.engine_version.clone(),
-                            html! {
-                                {env!("CARGO_PKG_VERSION")}
-                            }
-                        ),
-                        (
-                            t.config.portal.title_suffix.clone(),
-                            html! {
-                                {global_config.portal.title_suffix.clone()}
-                            }
-                        ),
-                        (
-                            t.config.portal.footer_banner.clone(),
-                            html! {
-                                global_config.portal.footer_banner.iter().map(|item| html! {
-                                    <a
-                                        class={css!("
+                    if let Some(global_config) = (*global_config).clone() {
+                        [
+                            (
+                                t.config.portal.engine_version.clone(),
+                                html! {
+                                    {env!("CARGO_PKG_VERSION")}
+                                }
+                            ),
+                            (
+                                t.config.portal.title_suffix.clone(),
+                                html! {
+                                    {global_config.portal.title_suffix.clone()}
+                                }
+                            ),
+                            (
+                                t.config.portal.footer_banner.clone(),
+                                html! {
+                                    global_config.portal.footer_banner.iter().map(|item| html! {
+                                        <a
+                                            class={css!("
+                                                width: 100%;
+                                                height: 32px;
+                                                padding: 0 16px;
+
+                                                font-size: 16px;
+                                                line-height: 32px;
+                                                text-align: center;
+                                                user-select: none;
+                                            ")}
+                                            href={item.url.clone().unwrap_or_default()}
+                                            target="_blank"
+                                        >
+                                            {item.text.clone()}
+                                        </a>
+                                    }).collect::<Html>()
+                                }
+                            ),
+                            (
+                                t.config.portal.language.clone(),
+                                html! {
+                                    {
+                                        global_config.portal.language.clone()
+                                    }
+                                },
+                            ),
+                            (
+                                t.config.portal.timezone.clone(),
+                                html! {
+                                    {
+                                        global_config.portal.timezone.to_string()
+                                    }
+                                },
+                            ),
+                            (
+                                t.config.router.media_entry_path.clone(),
+                                html! {
+                                    {
+                                        global_config.router.media_entry_path.clone()
+                                    }
+                                },
+                            ),
+                            (
+                                t.config.router.limit_referrer_host.clone(),
+                                {
+                                    if let Some(val) = &global_config.router.limit_referrer_host {
+                                        if !val.is_empty() {
+                                            return Ok(val.iter().map(|item| html! {
+                                                <a
+                                                    class={css!("
+                                                        width: 100%;
+                                                        height: 32px;
+                                                        padding: 0 16px;
+
+                                                        font-size: 16px;
+                                                        line-height: 32px;
+                                                        text-align: center;
+                                                        user-select: none;
+                                                    ")}
+                                                    href={item.clone()}
+                                                    target="_blank"
+                                                >
+                                                    {item.clone()}
+                                                </a>
+                                            }).collect::<Html>());
+                                        }
+                                    }
+                                    html! {
+                                        <p class={css!("
                                             width: 100%;
                                             height: 32px;
-                                            padding: 0 16px;
 
                                             font-size: 16px;
                                             line-height: 32px;
                                             text-align: center;
+                                            font-style: italic;
                                             user-select: none;
-                                        ")}
-                                        href={item.url.clone().unwrap_or_default()}
-                                        target="_blank"
-                                    >
-                                        {item.text.clone()}
-                                    </a>
-                                }).collect::<Html>()
-                            }
-                        ),
-                        (
-                            t.config.portal.language.clone(),
-                            html! {
-                                {
-                                    global_config.portal.language.clone()
-                                }
-                            },
-                        ),
-                        (
-                            t.config.portal.timezone.clone(),
-                            html! {
-                                {
-                                    global_config.portal.timezone.to_string()
-                                }
-                            },
-                        ),
-                        (
-                            t.config.router.media_entry_path.clone(),
-                            html! {
-                                {
-                                    global_config.router.media_entry_path.clone()
-                                }
-                            },
-                        ),
-                        (
-                            t.config.router.limit_referrer_host.clone(),
-                            {
-                                if let Some(val) = &global_config.router.limit_referrer_host {
-                                    if !val.is_empty() {
-                                        return Ok(val.iter().map(|item| html! {
-                                            <a
-                                                class={css!("
-                                                    width: 100%;
-                                                    height: 32px;
-                                                    padding: 0 16px;
-
-                                                    font-size: 16px;
-                                                    line-height: 32px;
-                                                    text-align: center;
-                                                    user-select: none;
-                                                ")}
-                                                href={item.clone()}
-                                                target="_blank"
-                                            >
-                                                {item.clone()}
-                                            </a>
-                                        }).collect::<Html>());
+                                        ")}>
+                                            {"None"}
+                                        </p>
                                     }
                                 }
+                            ),
+                            (
+                                t.config.upload.image_size_limit.clone(),
                                 html! {
-                                    <p class={css!("
-                                        width: 100%;
-                                        height: 32px;
+                                    {
+                                        global_config.upload.image_size_limit.clone()
+                                    }
+                                },
+                            ),
+                            (
+                                t.config.upload.webp_auto_convert.clone(),
+                                html! {
+                                    if global_config.upload.webp_auto_convert {
+                                        {"true"}
+                                    } else {
+                                        {"false"}
+                                    }
+                                },
+                            ),
+                            (
+                                t.config.upload.use_source_file_name.clone(),
+                                html! {
+                                    if global_config.upload.use_source_file_name {
+                                        {"true"}
+                                    } else {
+                                        {"false"}
+                                    }
+                                },
+                            ),
+                        ].iter().map(|(name, value)| html! {
+                            <div class={css!("
+                                width: 100%;
+                                min-height: 64px;
 
-                                        font-size: 16px;
-                                        line-height: 32px;
-                                        text-align: center;
-                                        font-style: italic;
-                                        user-select: none;
-                                    ")}>
-                                        {"None"}
-                                    </p>
-                                }
-                            }
-                        ),
-                        (
-                            t.config.upload.image_size_limit.clone(),
-                            html! {
-                                {
-                                    global_config.upload.image_size_limit.clone()
-                                }
-                            },
-                        ),
-                        (
-                            t.config.upload.webp_auto_convert.clone(),
-                            html! {
-                                if global_config.upload.webp_auto_convert {
-                                    {"true"}
-                                } else {
-                                    {"false"}
-                                }
-                            },
-                        ),
-                        (
-                            t.config.upload.use_source_file_name.clone(),
-                            html! {
-                                if global_config.upload.use_source_file_name {
-                                    {"true"}
-                                } else {
-                                    {"false"}
-                                }
-                            },
-                        ),
-                    ].iter().map(|(name, value)| html! {
-                        <div class={css!("
-                            width: 100%;
-                            min-height: 64px;
-
-                            display: flex;
-                            align-items: center;
-                            justify-content: space-between;
-                        ")}>
-                            <span class={css!("
-                                width: 20%;
-                                min-width: 128px;
-                                height: 32px;
-
-                                font-size: 16px;
-                                line-height: 32px;
-                                user-select: none;
+                                display: flex;
+                                align-items: center;
+                                justify-content: space-between;
                             ")}>
-                                {name.to_string()}
-                            </span>
-                            <span
-                                class={css!("
-                                    width: 80%;
-                                    min-height: 32px;
-                                    padding: 0 16px;
-
-                                    border: none;
-                                    outline: 1px solid var(--color-light-half);
+                                <span class={css!("
+                                    width: 20%;
+                                    min-width: 128px;
+                                    height: 32px;
 
                                     font-size: 16px;
                                     line-height: 32px;
                                     user-select: none;
+                                ")}>
+                                    {name.to_string()}
+                                </span>
+                                <span
+                                    class={css!("
+                                        width: 80%;
+                                        min-height: 32px;
+                                        padding: 0 16px;
 
-                                    display: flex;
-                                    flex-direction: column;
-                                    align-items: center;
-                                    justify-content: center;
+                                        border: none;
+                                        outline: 1px solid var(--color-light-half);
 
-                                    background: var(--color-dark-less);
-                                    border-radius: 4px;
-                                    box-shadow: var(--shadow-half);
-                                ")}
-                            >
-                                {value.clone()}
-                            </span>
-                        </div>
-                    }).collect::<Html>()
+                                        font-size: 16px;
+                                        line-height: 32px;
+                                        user-select: none;
+
+                                        display: flex;
+                                        flex-direction: column;
+                                        align-items: center;
+                                        justify-content: center;
+
+                                        background: var(--color-dark-less);
+                                        border-radius: 4px;
+                                        box-shadow: var(--shadow-half);
+                                    ")}
+                                >
+                                    {value.clone()}
+                                </span>
+                            </div>
+                        }).collect::<Html>()
+                    } else {
+                        html! {}
+                    }
                 }
             </div>
         </div>
--- packages/types/src/config.rs
+++ packages/types/src/config.rs
@@ -1,52 +1,32 @@
 use anyhow::{anyhow, Result};
 use once_cell::sync::Lazy;
-use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
+
+use yuuka::derive_config;
 
 use crate::consts::CONFIG_DIR;
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
-#[serde(rename_all = "kebab-case")]
-pub struct Config {
-    pub portal: config_item::Portal,
-    pub router: config_item::Router,
-    pub upload: config_item::Upload,
-}
-
-pub mod config_item {
-    use serde::{Deserialize, Serialize};
-
-    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
-    #[serde(rename_all = "kebab-case")]
-    pub struct Portal {
-        pub title_suffix: String,
-        pub footer_banner: Vec<FooterBannerItem>,
-        pub language: String,
-        pub timezone: i32,
+derive_config!(Config {
+    portal: Portal {
+        title_suffix: String,
+        footer_banner: [FooterBannerItem {
+            text: String,
+            url: Option<String>,
+        }],
+        language: String,
+        timezone: i32,
+    },
+    router: Router {
+        media_entry_path: String,
+        limit_referrer_host: Option<Vec<String>>,
+    },
+    upload: Upload {
+        image_size_limit: String,
+        webp_auto_convert: bool,
+        use_source_file_name: bool,
     }
-
-    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
-    #[serde(rename_all = "kebab-case")]
-    pub struct FooterBannerItem {
-        pub text: String,
-        pub url: Option<String>,
-    }
-
-    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
-    #[serde(rename_all = "kebab-case")]
-    pub struct Router {
-        pub media_entry_path: String,
-        pub limit_referrer_host: Option<Vec<String>>,
-    }
-
-    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
-    #[serde(rename_all = "kebab-case")]
-    pub struct Upload {
-        pub image_size_limit: String,
-        pub webp_auto_convert: bool,
-        pub use_source_file_name: bool,
-    }
-}
+});
+pub use __Config::*;
 
 pub static CONFIG: Lazy<Arc<Mutex<Config>>> = Lazy::new(|| {
     let raw = std::fs::read_to_string(CONFIG_DIR.clone()).unwrap();
