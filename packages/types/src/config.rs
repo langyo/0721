032a972573b8600f7bfd9fe486adad64c6b60c46
伊欧 <langyo.china@gmail.com>
2024-10-09 11:17:56 +0800

    ⬆️ Bump `yuuka`.

--- packages/types/src/config.rs
+++ packages/types/src/config.rs
@@ -2,31 +2,30 @@ use anyhow::{anyhow, Result};
 use once_cell::sync::Lazy;
 use std::sync::{Arc, Mutex};
 
-use yuuka::derive_config;
+use yuuka::derive_struct;
 
 use crate::consts::CONFIG_DIR;
 
-derive_config!(Config {
-    portal: Portal {
+derive_struct!(pub Config {
+    portal: {
         title_suffix: String,
-        footer_banner: [FooterBannerItem {
+        footer_banner: [{
             text: String,
             url: Option<String>,
         }],
         language: String,
         timezone: i32,
     },
-    router: Router {
+    router: {
         media_entry_path: String,
         limit_referrer_host: Option<Vec<String>>,
     },
-    upload: Upload {
+    upload: {
         image_size_limit: String,
         webp_auto_convert: bool,
         use_source_file_name: bool,
     }
 });
-pub use __Config::*;
 
 pub static CONFIG: Lazy<Arc<Mutex<Config>>> = Lazy::new(|| {
     let raw = std::fs::read_to_string(CONFIG_DIR.clone()).unwrap();
