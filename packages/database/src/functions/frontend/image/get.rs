2d63610423c175a1c09bfd586de28ef32d7f3f03
伊欧 <langyo.china@gmail.com>
2024-10-12 20:47:13 +0800

    🚑 Allow visit anonymous image directly.

--- packages/database/src/functions/frontend/image/get.rs
+++ packages/database/src/functions/frontend/image/get.rs
@@ -13,31 +13,37 @@ pub async fn get_file(
     auth: AuthInfo,
     db_key: impl ToString,
 ) -> Result<(String, Bytes)> {
-    let item = get(env.clone(), db_key.to_string())
-        .await?
-        .ok_or(anyhow!("Image not found"))?;
+    if let Some(item) = get(env.clone(), db_key.to_string()).await? {
+        let mime = ImageFormat::from_mime_type(&item.mime)
+            .ok_or(anyhow!("Failed to get MIME type from image"))?;
 
-    // Check permission
-    if let Some(item_permission) = item.permission {
-        if let Some(auth) = auth {
-            ensure!(auth.permission >= item_permission, "No permission");
+        let path = MEDIA_DIR.clone();
+        let path = path.join(format!(
+            "{}.{}",
+            item.name,
+            mime.extensions_str().first().ok_or(anyhow!(
+                "Failed to get extension from MIME type: {}",
+                mime.to_mime_type()
+            ))?
+        ));
+
+        let file = tokio::fs::read(path).await?;
+        let file = Bytes::from(file);
+
+        // Check permission
+        if let Some(item_permission) = item.permission {
+            if let Some(auth) = auth {
+                ensure!(auth.permission >= item_permission, "No permission");
+            }
         }
+
+        Ok((item.mime, file))
+    } else {
+        let path = MEDIA_DIR.clone();
+        let path = path.join(format!("{}.webp", db_key.to_string()));
+
+        let file = tokio::fs::read(path).await?;
+        let file = Bytes::from(file);
+        Ok(("image/webp".to_string(), file))
     }
-
-    let mime = ImageFormat::from_mime_type(&item.mime)
-        .ok_or(anyhow!("Failed to get MIME type from image"))?;
-    let path = MEDIA_DIR.clone();
-    let path = path.join(format!(
-        "{}.{}",
-        item.name,
-        mime.extensions_str().first().ok_or(anyhow!(
-            "Failed to get extension from MIME type: {}",
-            mime.to_mime_type()
-        ))?
-    ));
-
-    let file = tokio::fs::read(path).await?;
-    let file = Bytes::from(file);
-
-    Ok((item.mime, file))
 }
